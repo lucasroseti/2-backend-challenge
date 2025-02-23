282e5c3ffe0c219ffdf613a4545d4b047932c5f0
Lucas Roseti <lc.roseti@gmail.com>
2025-02-23 18:44:03 -0300

    Improving routes and adjusts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,31 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
-import { immutability } from '../typescript/immutability'
-import { typeGenerics } from '../typescript/type-generics'
-import { unionTypes } from '../typescript/union-types'
-
-interface TypeGenericsParams {
-  field: string
-}
+import { typeScriptRoutes } from './typescript'
 
 export async function routes(app: FastifyInstance) {
-  app.get('/type-generics/:field', async (req, res) => {
-    const { field } = req.params as TypeGenericsParams
-
-    const data = typeGenerics(field)
-
-    return res.status(200).send(data)
-  }),
-
-  app.get('/type-union', async (_req, res) => {
-    const data = unionTypes()
-
-    return res.status(200).send(data)
-  }),
-
-  app.get('/immutability', async (_req, res) => {
-    const data = immutability()
-
-    return res.status(200).send(data)
-  })
+  app.register(typeScriptRoutes, { prefix: '/typescript' })
 }
--- src/typescript/immutability.ts
+++ src/typescript/immutability.ts
@@ -8,7 +8,7 @@
 import { makeAllPositive } from '../functions/makeAllPositive'
 
 export function immutability() {
-  const numbers = [-1, 2, -3, 4];
+  const numbers = [-1, 2, -3, 4]
   const result = makeAllPositive(numbers)
 
   return { numbers, result }
